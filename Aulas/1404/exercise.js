2aa0c4788c637e1f14b5cf2d1aa5e29d4abb8064
Larissa Rabello <lari.rabello@gmail.com>
2016-04-14 22:12:05 -0300

    including 04-14 class

---
+++ Aulas/1404/exercise.js
@@ -0,0 +1,12 @@
+var valor1=prompt("Insira o Valor 1");
+var valor2=prompt("Insira o Valor 2");
+
+
+if (valor1 > valor2) {
+var maior = valor1;
+
+} else {
+var maior = valor2;
+}
+
+alert ("O triplo do maior valor é  "+3*maior);
---
+++ Aulas/1404/exercise_complete.js
@@ -0,0 +1,19 @@
+while (valor1==valor2) {
+  var valor1=prompt("Insira o Valor1");
+  var valor2=prompt("Insira o Valor2");
+}
+
+if (valor1>=0) && (valor1<=1000) {
+  var a=valor1;
+} else { var valor1=prompt("Número Inválido. Insira o Valor1");
+
+}
+if (valor1 > valor2) {
+  var maior = valor1;
+
+} else {
+  var maior = valor2;
+}
+
+alert ("O triplo do maior valor é  "+3*maior);
+/*
